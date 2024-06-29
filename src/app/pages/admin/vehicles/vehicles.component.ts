8597aad910feeefdabad3d1ee5a73afd17352a99
matiasolocco <matias.olocco@hotmail.com>
2024-06-29 12:29:39 +0200

    admin/vehicles components init (loading only)

--- src/app/pages/admin/vehicles/vehicles.component.ts
+++ src/app/pages/admin/vehicles/vehicles.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { Vehicle } from '../../../interfaces/vehicle';
+import { VehicleService } from '../../../services/vehicle.service';
+import { DivisaPipe } from '../../../pipes/divisa.pipe';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { FilterVehiclesPipe } from '../../../pipes/filter-vehicles.pipe';
 
 @Component({
   selector: 'app-vehicles',
   standalone: true,
-  imports: [],
+  imports: [DivisaPipe, RouterModule, FormsModule, FilterVehiclesPipe],
   templateUrl: './vehicles.component.html',
-  styleUrl: './vehicles.component.css'
+  styleUrls: ['./vehicles.component.css']
 })
 export class VehiclesComponent {
+  vehicles: Vehicle[] = [];
+  filtro: string = "";
+  selectedVehicle: Vehicle | null = null;
 
+  constructor(private vehicleService: VehicleService) {
+    this.loadVehicles();
+  }
+
+  loadVehicles() {
+    this.vehicleService.getAll().subscribe({
+      next: (response) => {
+        this.vehicles = response as Vehicle[];
+      },
+      error: (error) => {
+        console.error('Error al cargar los vehículos', error);
+      }
+    });
+  }
 }
