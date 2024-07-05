7ac4429b05d5dc2dff59ec738ee025869c55d8d3
matiasolocco <matias.olocco@hotmail.com>
2024-07-05 20:05:38 +0200

    Admin/Vehicle modify WORKS

--- src/app/services/vehicle.service.ts
+++ src/app/services/vehicle.service.ts
@@ -1,30 +1,38 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Vehicle } from '../interfaces/vehicle';
-
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehicleService {
+  private url = 'http://localhost:3000/api/vehicles';
 
-  url : string = "http://localhost:3000/api/vehicles"
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  private getAuthHeaders(): HttpHeaders {
+    const user = this.authService.getUser();
+    const token = user ? user.token : null;
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+  }
 
   getAll(): Observable<Vehicle[]> {
-    return this.http.get<Vehicle[]>(this.url);
+    return this.http.get<Vehicle[]>(this.url, { headers: this.getAuthHeaders() });
   }
 
   getById(id: string): Observable<Vehicle> {
-    return this.http.get<Vehicle>(`${this.url}/${id}`);
+    return this.http.get<Vehicle>(`${this.url}/${id}`, { headers: this.getAuthHeaders() });
   }
 
-  updateVehicle(id: string, vehicle: Partial<Vehicle>): Observable<Vehicle> {
-    return this.http.patch<Vehicle>(`${this.url}/${id}`, vehicle);
+  updateVehicle(id: string, vehicle: Vehicle): Observable<any> {
+    return this.http.patch(`${this.url}/${id}`, vehicle, { headers: this.getAuthHeaders() });
   }
 
-  deleteVehicle(id: string): Observable<void> {
-    return this.http.delete<void>(`${this.url}/${id}`);
+  deleteVehicle(id: string): Observable<any> {
+    return this.http.delete(`${this.url}/${id}`, { headers: this.getAuthHeaders() });
   }
 }
